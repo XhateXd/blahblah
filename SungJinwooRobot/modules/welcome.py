72e9326df3ec5d7d74b0f7075cbc412a17478828
Hirojazz <79592216+Hirojazz@users.noreply.github.com>
2021-06-03 22:50:25 +0530

    Update welcome.py

--- SungJinwooRobot/modules/welcome.py
+++ SungJinwooRobot/modules/welcome.py
@@ -188,7 +188,7 @@ def new_member(update: Update, context: CallbackContext):
             elif new_mem.id in DEV_USERS:
                 update.effective_message.reply_text(
                     "Whoa! A Kingdom Of Science Senior just joined! Position: [Science User Chrome](https://t.me/senkubotupdates/4)",
-                    parse_mode=ParseMode.MARKDOWN
+                    parse_mode=ParseMode.MARKDOWN, 
                     reply_to_message_id=reply, 
                 )
                 continue
