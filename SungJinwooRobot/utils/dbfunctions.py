9a6d78e70d04320a6b6092a47296305aae9a7832
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2021-06-16 13:27:39 +0530

    Update dbfunctions.py

--- SungJinwooRobot/utils/dbfunctions.py
+++ SungJinwooRobot/utils/dbfunctions.py
@@ -65,3 +65,27 @@ async def alpha_to_int(user_id_alphabet: str) -> int:
         user_id += str(index)
     user_id = int(user_id)
     return user_id
+
+# Couple Chooser
+async def _get_lovers(chat_id: int):
+    lovers = await coupledb.find_one({"chat_id": chat_id})
+    if not lovers:
+        return {}
+    return lovers["couple"]
+
+
+async def get_couple(chat_id: int, date: str):
+    lovers = await _get_lovers(chat_id)
+    if date in lovers:
+        return lovers[date]
+    else:
+        return False
+
+
+async def save_couple(chat_id: int, date: str, couple: dict):
+    lovers = await _get_lovers(chat_id)
+    lovers[date] = couple
+    await coupledb.update_one(
+        {"chat_id": chat_id}, {"$set": {"couple": lovers}}, upsert=True
+    )
+
