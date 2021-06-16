52016634e2130b7826dc94f0657a28c2edc8e280
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2021-06-16 14:30:10 +0530

    Update mongo.py

--- SungJinwooRobot/mongo.py
+++ SungJinwooRobot/mongo.py
@@ -5,7 +5,7 @@ from motor import motor_asyncio
 from SungJinwooRobot import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from SungJinwooRobot.conf import get_int_key, get_str_key
+from SungJinwooRobot.config import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
