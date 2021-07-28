da861d7a21cc568db924662c47ebe38f8fa9ec4a
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2021-07-29 01:16:57 +0530

    Update fun.py

--- SungJinwooRobot/modules/fun.py
+++ SungJinwooRobot/modules/fun.py
@@ -40,9 +40,10 @@ def sanitize(update: Update, context: CallbackContext):
 def slap(update: Update, context: CallbackContext):
     message = update.effective_message
     name = message.reply_to_message.from_user.first_name if message.reply_to_message else message.from_user.first_name
+    first = message.from_user.first_name
     reply_animation = message.reply_to_message.reply_animation if message.reply_to_message else message.reply_animation
     reply_animation(
-        random.choice(fun_strings.SLAPS), caption=f'*{first_Name} slaps {name}*')
+        random.choice(fun_strings.SLAPS), caption=f'*{first} slaps {name}*')
 
 
 @run_async
