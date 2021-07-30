d0fdf8bb4b86ee01ab2218873a54c3ab8cab049a
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2021-07-31 01:29:39 +0530

    Update AI_Chat.py

--- SungJinwooRobot/modules/AI_Chat.py
+++ SungJinwooRobot/modules/AI_Chat.py
@@ -23,8 +23,7 @@ import re
 
 import aiohttp
 
-# from google_trans_new import google_translator
-from googletrans import Translator as google_translator
+from google_trans_new import google_translator
 from pyrogram import filters
 
 from SungJinwooRobot import BOT_ID
