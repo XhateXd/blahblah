5713bdefc08efff0c770459b8ff44a325144c20a
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2021-07-31 12:56:50 +0530

    Update dbfunctions.py

--- SungJinwooRobot/utils/dbfunctions.py
+++ SungJinwooRobot/utils/dbfunctions.py
@@ -1,11 +1,11 @@
-from SungJinwooRobot import db
+from SungJinwooRobot.services.mongo import db
 from typing import Dict, List, Union
 
-karmadb = db.karma
+repdb = db.rep
 coupledb=db.couple
 
 async def get_karmas_count() -> dict:
-    chats = karmadb.find({"chat_id": {"$lt": 0}})
+    chats = repdb.find({"chat_id": {"$lt": 0}})
     if not chats:
         return {}
     chats_count = 0
@@ -21,7 +21,7 @@ async def get_karmas_count() -> dict:
 
 
 async def get_karmas(chat_id: int) -> Dict[str, int]:
-    karma = await karmadb.find_one({"chat_id": chat_id})
+    karma = await repdb.find_one({"chat_id": chat_id})
     if karma:
         karma = karma['karma']
     else:
@@ -40,7 +40,7 @@ async def update_karma(chat_id: int, name: str, karma: dict):
     name = name.lower().strip()
     karmas = await get_karmas(chat_id)
     karmas[name] = karma
-    await karmadb.update_one(
+    await repdb.update_one(
         {"chat_id": chat_id},
         {
             "$set": {
